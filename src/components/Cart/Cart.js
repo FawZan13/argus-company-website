c69132ec1c2452487a056202a3ccda08fbeb34f1
FawZan13 <fawzanbinmokhtar@gmail.com>
2021-09-26 19:29:48 +0600

    half

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,9 +4,9 @@ import './Cart.css';
 const Cart = () => {
     return (
         <div className="cart ">
-            <h5 className="p-2">Pokemon Added:</h5>
+            <h5 className="p-2">Employee Added:</h5>
             <h5 className="p-2">Total Cost:</h5>
-            <h5 className="p-2">Party Member:</h5>
+            <h5 className="p-2">Employees:</h5>
         </div>
     );
 };
